ba025266ec9f2d9f9596641bb674e01f89eb887f
Brayhan Oliveros <brayhan.oliveros@gmail.com>
2025-02-13 10:16:20 -0400

    feat: add coverage configuration for Vitest and update .gitignore

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,13 +5,25 @@ export default defineConfig({
         environment: 'jsdom',
         globals: true,
         setupFiles: ['./src/setupTests.ts'],
-        exclude: ['./src/hooks/useOrderSpy.test.ts',
-            './src/hooks/useOrder.test.ts',
-            './src/hooks/useOrderMsw.test.tsx',
-            '**/node_modules/**',
-            '**/dist/**',
-            '**/cypress/**',
-            '**/.{idea,git,cache,output,temp}/**',
-            './src/config/**',],
+        exclude:
+            ['./src/hooks/useOrderSpy.test.ts',
+                './src/hooks/useOrder.test.ts',
+                './src/hooks/useOrderMsw.test.tsx',
+                '**/node_modules/**',
+                '**/dist/**',
+                '**/cypress/**',
+                '**/.{idea,git,cache,output,temp}/**',
+                './src/config/**',
+            ],
+        coverage: {
+            exclude: [
+                '**/*.config.[jt]s',
+                '**/*.types.ts',
+                '**/*.d.ts',
+                '**/types',
+                '**/config',
+                '**/mocks',
+            ]
+        }
     },
 });
