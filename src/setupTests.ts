3eb21426af0a041d6c3c43971a4a2e2388b05c14
Brayhan Oliveros <brayhan.oliveros@gmail.com>
2025-02-13 09:10:13 -0400

    feat: add mock server and handlers for order data fetching

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,15 @@
 import '@testing-library/jest-dom'
+import { afterAll, afterEach, beforeAll } from 'vitest'
+import { server } from './mocks/servers'
+
+beforeAll(() => {
+    server.listen()
+})
+
+afterAll(() => {
+    server.close()
+})
+
+afterEach(() => {
+    server.resetHandlers()
+})
