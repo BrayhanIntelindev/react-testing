bba8cad29a317dc008b8e495882fccfac1ff8f3a
Brayhan Oliveros <brayhan.oliveros@gmail.com>
2025-02-13 09:14:40 -0400

    feat: add tests for useOrder hook with error handling and mock implementations

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,14 @@ export default defineConfig({
     test: {
         environment: 'jsdom',
         globals: true,
-        setupFiles:['./src/setupTests.ts'],
+        setupFiles: ['./src/setupTests.ts'],
+        exclude: ['./src/hooks/useOrderSpy.test.ts',
+            './src/hooks/useOrder.test.ts',
+            './src/hooks/useOrderMsw.test.ts',
+            '**/node_modules/**',
+            '**/dist/**',
+            '**/cypress/**',
+            '**/.{idea,git,cache,output,temp}/**',
+            './src/config/**',],
     },
 });
